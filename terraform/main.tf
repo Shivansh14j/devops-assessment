d67b35f564c3201ea7889714e99a8cf1c92765db
Shivansh14j <bha.shivansh14@gmail.com>
2026-03-09 16:34:22 +0530

    Fix key_name formatting in aws_instance resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "devops_ec2" {
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t3.micro"
 
-  key_name = ho
+  key_name = "ho"
 
   vpc_security_group_ids = [
     aws_security_group.devops_sg.id
